4ef9d96fd97117f369ed51728cbebb25f1d9250f
stephen remmi <stephenremmi@gmail.com>
2020-04-03 23:02:01 +0300

    Add index.html

---
+++ js/scripts.js
@@ -0,0 +1,86 @@
+//get submit button
+let submit = document.getElementById('submit')
+submit.addEventListener('click',submit)
+
+//Callback function for submit
+function submit (e){
+  e.preventDefault()
+}
+var name = document.getElementById("name").value
+var year = document.getElementById("year").value
+var month = document.getElementById("month").value
+var date = document.getElementById("date").value
+
+var mydata = new Date();
+var day = mydate.getday();
+var month = mydate.getMonth();
+var year = mydate.getYear();
+var d = mydate.getDate();
+};
+
+//Akan name for male
+var day = 1
+switch (day) {
+  case 1:
+  document.write{
+  alert('Your name is Kwasi');}
+    break;
+    case 2:
+    document.write{
+    alert("Your name is Kwadwo");}
+    case 3:
+    break;
+    document.write{
+      alert("Your name is Kwabena");}
+    break;
+    case 4:
+    document.write{
+    alert{("Your name is Kwaku");}
+    break;
+    case 5:
+    document.write{
+    alert("Your name is Yaw");}
+    break;
+    case 6:
+    document.write{
+    alert("Your name is Kofi");}
+    break;
+    case 7:
+    document.write{
+    alert("Your name is Kwame");}
+  default:("Wrong Input")
+}
+//Akan name for female
+
+var day = 1
+switch(day) {
+  case 1:
+    document.write{
+    alert("Your name is Akwosua");}
+  break;
+  case 2:
+    document.write{
+    alert("Your name is Adwoa");}
+    break;
+    case 3:
+    document.write{
+    alert("Your name is Abenaa");}
+    break;
+    case 4:
+    document.write{
+    alert("Your name is Akua");}
+    break;
+    case 5:
+    document.write{
+    alert("Your name is Yaa");}
+    break;
+    case 6:
+    document.write{
+    alert("Your name is Afua");}
+    break;
+    case 7:
+    document.write{
+    alert("Your name is Ama");}
+    break;
+    default:("Wrong input")
+}

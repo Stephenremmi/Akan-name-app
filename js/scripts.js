5e23a36b1973e7eb78dfc7e27966cd257440e1fa
stephen remmi <stephenremmi@gmail.com>
2020-04-09 23:22:43 +0300

    Add final files

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,35 @@
 function buttonClick(){
-    var bdate = document.getElementById("bdate").value;
-    var date = new Date(bdate).getDay();
+    var birthDate = document.getElementById("birthDate").value;
+    var date = new Date(birthDate).getDay();
 
-    var male1 = document.getElementById("male1");
-    var female1 = document.getElementById("female1");
+    var male = document.getElementById("male");
+    var female = document.getElementById("female");
 
-    if(male1.checked==true){
-        const bnames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
-        const result = bnames[date];
-        alert("Your Akan name is "+result)
+    if(male.checked==true){
+        const maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+        const result = maleNames[date];
+        swal("Congratulations!","Your Akan name is " + result)
+        
     }
+        
 
-    else if(female1.checked==true){
-        const gnames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
-        const result = gnames[date];
-        alert("Your Akan name is "+result)
+    else if(female.checked==true){
+        const femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+        const result = femaleNames[date];
+        swal("congratulations!","Your Akan name is " + result)
     }
 
     else{
-        alert("Gender not submitted");
+        swal({
+            title:"Oops!!",
+            text: "Enter the empty fields",
+            icon: "warning",
+          });
     }
     
-}
+};
+        swal({        
+            title:"Welcome!!",
+            text: "AKAN WEB APP (2020)",
+            icon: "success",
+        })
